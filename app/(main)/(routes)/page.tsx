ae379b986a31bad83571b9f75e198ce3be868dfb
novipro88 <noviantipali97@gmail.com>
2024-05-17 23:49:18 +0700

    Dark & Light theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="p-2">
+    <div className="py-2">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
