ddb13be324a755ce1879c5ac831a900515673fdf
novipro88 <noviantipali97@gmail.com>
2024-05-19 10:48:50 +0700

    Fix code updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(font.className, "bg-white dark:bg-slate-700")}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
--- components/media-room.tsx
+++ components/media-room.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { LiveKitRoom, VideoConference } from "@livekit/components-react";
 import "@livekit/components-styles";
+import { Channel } from "@prisma/client"
 import { useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 
